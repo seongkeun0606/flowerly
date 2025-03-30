d9043d61fa45078e978cb8cf624370367f00afe0
seongkeun <wkdrnslasla1@naver.com>
2025-03-30 20:14:08 +0900

    chore: 프로젝트 초기 설정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+import * as SplashScreen from 'expo-splash-screen';
+
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import React from 'react';
+import { Stack } from 'expo-router';
 import { useColorScheme } from '@/components/useColorScheme';
+import { useEffect } from 'react';
+import { useFonts } from 'expo-font';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -29,7 +32,9 @@ export default function RootLayout() {
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
-    if (error) throw error;
+    if (error) {
+      throw error;
+    }
   }, [error]);
 
   useEffect(() => {
